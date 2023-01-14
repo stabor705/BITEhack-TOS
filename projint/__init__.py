d089a44e99e20e06df8dbce6c71b9ada88253f47
Ivan Zarzhitski <ivan.zarzhitski@gmail.com>
2023-01-14 17:09:22 +0100

    Add db schema and generation

--- projint/__init__.py
+++ projint/__init__.py
@@ -1,9 +1,10 @@
 import os
 
-from flask import Flask
-
+from flask import Flask, g
 def create_app(test_config = None):
     app = Flask(__name__, instance_relative_config = True)
 
+    from . import db
+    db.init_app(app)
 
     return app
