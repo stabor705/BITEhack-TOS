f6520a746d8126ec0fc9faed20b6caa47cb199d2
MCzech3605 <mczech@student.agh.edu.pl>
2023-01-14 18:46:36 +0100

    SQL Inserts for Ivan

---
+++ insertsA.sql
@@ -0,0 +1,14 @@
+INSERT INTO ans_windows (id, answers, window_number);
+VALUES
+(0,'c',1),
+(1,'ax + c',1),
+(2,'\frac{x^{r+1}}{r+1} + c',1),
+(3,'lnx + c',1),
+(4,'\frac{a^x}{lna} + c',1),
+(5,'e^x + c',1),
+(6,'cosx + c',1),
+(7,'-sinx + c',1),
+(8,'tgx + c',1),
+(9,'-ctgx + c',1),
+(10,'arctgx + c',1),
+(11,'arcsinx + c',1);
